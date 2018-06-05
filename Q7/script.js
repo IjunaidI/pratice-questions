d33a90ce7482112d659c5b0dead44c02cf4d250f
IjunaidI <mjunaid7843@gmail.com>
2018-06-05 16:51:45 +0500

    first commit

---
+++ Q7/script.js
@@ -0,0 +1,11 @@
+let isEven = 10;
+
+
+    if (value%2 == 0)
+    {
+    	console.log("EVEN")
+            
+    	}
+else{
+	console.log("ODD")
+}
---
+++ Q8/script.js
@@ -0,0 +1,11 @@
+let yearDays = 366;
+
+
+if(yearDays == 366)
+{
+	console.log("this a leap year")
+}
+else
+{
+	console.log("this is not a leap year")
+}
